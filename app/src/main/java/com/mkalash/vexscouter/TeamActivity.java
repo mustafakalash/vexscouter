3ea407121925851518ecc6952ccfc01e618b404e
Mustafa Kalash <me@mkalash.com>
2016-12-04 16:23:04 -0500

    Fixes #1. Having a parent and an up would probably complicate implenting #4, anyway.

--- app/src/main/java/com/mkalash/vexscouter/TeamActivity.java
+++ app/src/main/java/com/mkalash/vexscouter/TeamActivity.java
@@ -8,17 +8,18 @@ import android.os.Bundle;
 public class TeamActivity extends AppCompatActivity {
 
     private String teamNumber;
+    private String eventSKU;
+    private String eventName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_team);
 
-        ActionBar ab = getSupportActionBar();
-        ab.setDisplayHomeAsUpEnabled(true);
-
         Intent intent = getIntent();
         teamNumber = intent.getStringExtra("TEAM_NUM");
+        eventSKU = intent.getStringExtra("EVENT_SKU");
+        eventName = intent.getStringExtra("EVENT_NAME");
         setTitle(teamNumber);
     }
 }
